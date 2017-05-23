f5d1ca1ccc37fab394c28da0acf1dc676afb0b86
mohankumar <mohankumar@mohan>
2017-05-23 11:51:57 +0530

    inital checkin of the palindrome

---
+++ Github/src/com/mccoy/main/PalindromeExample.java
@@ -0,0 +1,14 @@
+package com.mccoy.main;
+
+import com.mccoy.logic.Palindrome;
+
+public class PalindromeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Palindrome p=new Palindrome();
+		p.Charpal(454);
+
+	}
+
+}
